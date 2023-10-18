1b380fc5734fb10f5dd737cc9f13a71127197184
retumaru <220109uj@yse-c.net>
2023-10-18 12:15:49 +0900

    課題

---
+++ server.js
@@ -0,0 +1,32 @@
+// expressモジュール読み込み
+const express = require('express')
+// dotenvモジュール読み込み
+const dotenv = require('dotenv')
+// routerモジュール読み込み
+const routes = require('./routes')
+
+// dotenvの設定読み込み
+dotenv.config()
+const HOST = process.env.HOST
+const PORT = process.env.PORT
+
+// サーバ作成
+const app = express()
+
+// ミドルウェアの設定
+// publicフォルダを静的コンテンツのフォルダに設定
+app.use(express.static(__dirname + '/public'))
+
+// URLエンコード
+app.use(express.urlencoded({ extended: true }))
+
+// ルーティングを有効
+app.use(routes)
+
+//　サーバ停止: 起動中のターミナルで Ctrl + C
+// サーバ待機（Listen）
+app.listen(PORT, HOST, () => {
+    console.log(HOST)
+    console.log(PORT)
+    console.log('wait...')
+})
